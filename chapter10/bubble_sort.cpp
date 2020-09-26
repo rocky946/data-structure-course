e8099ffb7bc1cfc6f3a0f532661a25f5ecce6e17
rocky <rocky946@outlook.com>
2020-09-26 12:14:09 +0800

    添加从后往前冒泡方法

--- chapter10/bubble_sort.cpp
+++ chapter10/bubble_sort.cpp
@@ -19,6 +19,26 @@ void BubbleSort(RecType R[], int n) {
     }
 }
 
+/**
+ * 冒泡排序（从后往前冒泡）
+ * @param R
+ * @param n
+ */
+void BubbleSort1(RecType R[], int n) {
+    int i, j;
+
+    for (i = 0; i < n - 1; i++) {
+        // 将R[i]元素归位
+        for (j = n - 1; j > i; j--) {
+            // 相邻两个元素反序时交换
+            if (R[j].key < R[j - 1].key) {
+                swap(R[j], R[j - 1]);
+            }
+        }
+        DispList(R, n);
+    }
+}
+
 int main() {
     int n = 10;
     RecType R[MAXL];
@@ -26,7 +46,7 @@ int main() {
     CreateList(R, a, n);
     printf("排序前:");
     DispList(R, n);
-    BubbleSort(R, n);
+    BubbleSort1(R, n);
     printf("排序后:");
     DispList(R, n);
 
